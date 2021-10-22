db99bb7694ee9905f10b25d022a8b1523353cabe
guryanovae <ekaterinaguryanova66@gmail.com>
2021-10-22 14:20:17 +0700

    project commit

---
+++ app/src/main/java/com/example/databaselist/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.databaselist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button bAdd, bDel, bEmail, bFIO, bID;
+    EditText
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
